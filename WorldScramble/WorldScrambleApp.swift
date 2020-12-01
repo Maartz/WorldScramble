a1bec27ebdac1392f5b3678bf3c1d4f5e0b2a2a1
William MARTIN <william.martin@hey.com>
2020-12-01 15:29:59 +0100

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by William Martin on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
